851e96027ecacc92fe866e3c33666cf1f9ae574a
emilia <emi1213galarza@gmail.com>
2024-10-26 06:50:37 -0500

    Add repository into cars

---
+++ src/main/java/com/wif/car_rental_system/cars/repositories/CarRepository.java
@@ -0,0 +1,11 @@
+package com.wif.car_rental_system.cars.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wif.car_rental_system.cars.domain.entities.CarEntity;
+
+@Repository
+public interface CarRepository extends JpaRepository<CarEntity, Long> {
+    
+}
