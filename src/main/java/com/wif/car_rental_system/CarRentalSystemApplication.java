cd3b2406517cb52e0a8117df5a8325971a846ba8
Chu <dzhu2409@gmail.com>
2024-10-25 21:46:49 -0500

    Remove dotenv dependency and work with spring boot configuration

--- src/main/java/com/wif/car_rental_system/CarRentalSystemApplication.java
+++ src/main/java/com/wif/car_rental_system/CarRentalSystemApplication.java
@@ -2,12 +2,14 @@ package com.wif.car_rental_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/.env")
 public class CarRentalSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarRentalSystemApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CarRentalSystemApplication.class, args);
+  }
 
 }
