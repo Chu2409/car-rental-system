e09d817e73924dd7daa75ebada3ddfa331d6aab5
emilia <emi1213galarza@gmail.com>
2024-10-26 06:51:55 -0500

    Add controller into cars

---
+++ src/main/java/com/wif/car_rental_system/cars/controllers/CarController.java
@@ -0,0 +1,10 @@
+package com.wif.car_rental_system.cars.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cars")
+public class CarController {
+    
+}
