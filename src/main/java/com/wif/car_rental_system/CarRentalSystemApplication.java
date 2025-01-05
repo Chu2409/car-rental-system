faab3ffe9a0c730c2c06419d1f6b273228e5241c
SrPabvliss <pablomartinvillacres@gmail.com>
2025-01-05 01:39:54 -0500

    chore: files for local deploy

--- src/main/java/com/wif/car_rental_system/CarRentalSystemApplication.java
+++ src/main/java/com/wif/car_rental_system/CarRentalSystemApplication.java
@@ -2,9 +2,17 @@ package com.wif.car_rental_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CarRentalSystemApplication {
+public class CarRentalSystemApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(CarRentalSystemApplication.class);
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(CarRentalSystemApplication.class, args);
   }
