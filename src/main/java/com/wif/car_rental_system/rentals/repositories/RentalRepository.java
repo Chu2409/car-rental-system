58d12634e9c7d65f32fba0182cfca70daec0a1c8
SrPabvliss <pablomartinvillacres@gmail.com>
2025-01-11 21:34:35 -0500

    chore: added payments and issues on mapping

--- src/main/java/com/wif/car_rental_system/rentals/repositories/RentalRepository.java
+++ src/main/java/com/wif/car_rental_system/rentals/repositories/RentalRepository.java
@@ -10,6 +10,8 @@ import com.wif.car_rental_system.rentals.domain.entities.RentalEntity;
 
 @Repository
 public interface RentalRepository extends JpaRepository<RentalEntity, Long> {
+
   List<RentalEntity> findAllByUserId(Long userId, Pageable pageable);
+
   Long countAllByUserId(Long userId);
 }
